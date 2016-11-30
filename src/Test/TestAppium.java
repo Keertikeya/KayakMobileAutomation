7e1897507b5a506e7ee5f352131b8a3b94995289
sagardafle <sagar.cdafle@gmail.com>
2016-11-29 16:55:08 -0800

    Get and change orientation

--- src/Test/TestAppium.java
+++ src/Test/TestAppium.java
@@ -1,11 +1,16 @@
 package Test;
 
 import org.testng.annotations.BeforeMethod;
+
+import java.io.FileInputStream;
 import java.net.URL;
+import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -16,14 +21,17 @@ import io.appium.java_client.android.AndroidDriver;
 
 public class TestAppium {
 
-	WebDriver driver;
+	private static RemoteWebDriver driver;
+	private static Properties capabilitiesValues;
 	
 	
 	@Test
 	public void setUp() throws Exception {
 
-		// Created object of DesiredCapabilities class.
-		DesiredCapabilities capabilities = new DesiredCapabilities();
+		DesiredCapabilities capabilities=DesiredCapabilities.android();
+		capabilitiesValues  = new Properties();
+		capabilitiesValues.load(new FileInputStream("testdata/capabilities.properties"));
+		capabilities.setCapability(CapabilityType.BROWSER_NAME,"");
 
 		// Set android deviceName desired capability. Set it Android Emulator.
 		capabilities.setCapability("deviceName", "emulator-5554");
@@ -51,24 +59,26 @@ public class TestAppium {
 		// Created object of RemoteWebDriver will all set capabilities.
 		// Set appium server address and port number in URL string.
 		// It will launch calculator app in emulator.
-		driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+	    driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+		
+//		driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
 		closeButton();
 		clickLooksGoodBtn();
 		checkIfHomeScreen();
-		System.out.println("*--*--*-- Current screen orientation Is : " + ((AppiumDriver) driver).getOrientation());
-		((AppiumDriver) driver).rotate(org.openqa.selenium.ScreenOrientation.LANDSCAPE);
-		//countUIElements();
+		System.out.println("*--*--*-- Current screen orientation Is : " + ((AndroidDriver) driver).getOrientation());
+		((AndroidDriver) driver).rotate(org.openqa.selenium.ScreenOrientation.LANDSCAPE);
+		countUIElements();
 		driver.quit();
 
 	}
 
-//	private void countUIElements() {
-//	    List<WebElement> elements = ( driver).findElementsByAndroidUIAutomator("new UiSelector().clickable(true)");
-//	    if(elements.size()>0)System.out.println(elements);
-//		
-//	}
+	private void countUIElements() {
+	    
+		
+	}
 
 	private void clickLooksGoodBtn() {
 		String looksGoodBtn = "android:id/button1";
@@ -89,9 +99,9 @@ public class TestAppium {
 		String homeScreenToolbar = "com.kayak.android:id/toolbar";
 		WebElement homeToolBar = driver.findElement((By.id(homeScreenToolbar)));
 		if(homeToolBar.isDisplayed()){
-			System.out.println("HomeScree present");
+			System.out.println("HomeScreen present");
 		} else{
-			System.out.println("HomeScree absent");
+			System.out.println("HomeScreen absent");
 		}		
 	}
 
